7f41a597696ae1144bbf6de7d1b318a17d63355b
Krystovirus <105981378+Krystovirus@users.noreply.github.com>
2022-10-10 10:16:13 -0400

    task 6

--- main.js
+++ main.js
@@ -109,4 +109,18 @@ inputRef.addEventListener("input", (event) => {
 });
 
 //task 6
+const validationInput = document.querySelector("#validation-input");
+
+validationInput.addEventListener("blur", (event) => {
+    if (validationInput.value.length === 6) {
+        validationInput.classList.add("valid");
+        validationInput.classList.remove("invalid");
+      } else if (validationInput.value.length === 0) {
+        validationInput.classList.remove("invalid");
+        validationInput.classList.remove("valid");
+      } else {
+        validationInput.classList.add("invalid");
+        validationInput.classList.remove("valid");
+      }
+});
 
